a0b76f76f2c43bc1f88e86621bda105549b2f67e
mrAlekD <31899692+mrAlekD@users.noreply.github.com>
2024-04-15 15:25:47 +0300

    added harpoon

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -111,9 +111,39 @@ local toggleterm = {
 	end,
 }
 
+local harpoon = {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = true,
+	init = function()
+		local harpoon = require("harpoon")
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<leader>hh", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		vim.keymap.set("n", "<leader>h1", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<leader>h2", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<leader>h3", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<leader>h4", function()
+			harpoon:list():select(4)
+		end)
+	end,
+}
+
 return {
 	dressing,
 	neotree,
 	undotree,
 	toggleterm,
+	harpoon,
 }
