d5dd1a503bcb820b7fe3f9de942044fea8f01b8e
mrAlekD <31899692+mrAlekD@users.noreply.github.com>
2024-04-16 13:24:31 +0300

    fixed codeium

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -1,22 +1,26 @@
 return {
 	"Exafunction/codeium.vim",
 	event = "BufEnter",
-	config = function()
+	init = function()
+		vim.g.codeium_no_map_tab = 1
 		vim.keymap.set("i", "<C-g>", function()
 			return vim.fn["codeium#Accept"]()
-		end, { expr = true })
+		end, { expr = true, silent = true })
 		vim.keymap.set("i", "<C-a>a", function()
 			return vim.fn["codeium#Accept"]()
-		end, { expr = true, desc = "Codeium Accept" })
+		end, { expr = true, desc = "Codeium Accept", silent = true })
 		vim.keymap.set("i", "<C-a>n", function()
 			vim.fn["codeium#CycleCompletions"](1)
-		end, { desc = "Codeium Next Completion" })
+		end, { desc = "Codeium Next Completion", silent = true })
 		vim.keymap.set("i", "<C-a>p", function()
 			vim.fn["codeium#CycleCompletions"](-1)
-		end, { desc = "Codeium Previous Completion" })
-		vim.keymap.set("i", "<C-a>x", function()
+		end, { desc = "Codeium Previous Completion", silent = true })
+		vim.keymap.set("i", "<C-a>c", function()
 			vim.fn["codeium#Clear"]()
-		end, { desc = "Codeium Clear" })
+		end, { desc = "Codeium Clear", silent = true })
+		vim.keymap.set("i", "<C-a>t", function()
+			vim.fn["codeium#Complete"]()
+		end, { desc = "Codeium Trigger", silent = true })
 		vim.keymap.set("n", "<Leader>;", function()
 			if vim.g.codeium_enabled == true then
 				vim.cmd("CodeiumDisable")
