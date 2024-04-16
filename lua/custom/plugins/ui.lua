d447de529844bbd8b4cd035136326e2ded990dc6
mrAlekD <31899692+mrAlekD@users.noreply.github.com>
2024-04-16 13:50:42 +0300

    removed neotree

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -3,59 +3,9 @@ local dressing = {
 	opts = {},
 }
 
-local neotree = {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	opts = {
-		filesystem = {
-			hijack_netrw_behavior = "open_current",
-		},
-		window = {
-			mappings = {
-				["e"] = function()
-					vim.cmd("Neotree focus filesystem left")
-				end,
-				["b"] = function()
-					vim.cmd("Neotree focus buffers left")
-				end,
-				["g"] = function()
-					vim.cmd("Neotree focus git_status left")
-				end,
-			},
-		},
-		event_handlers = {
-			{
-				event = "file_opened",
-				handler = function()
-					require("neo-tree.command").execute({ action = "close" })
-				end,
-			},
-		},
-	},
-	init = function()
-		vim.keymap.set(
-			"n",
-			"<leader>e",
-			"<CMD>Neotree toggle<CR>",
-			{ noremap = true, silent = true, desc = "NeoTree Toggle" }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>o",
-			"<CMD>Neotree focus<CR>",
-			{ noremap = true, silent = true, desc = "Focus NeoTree" }
-		)
-	end,
-}
-
 local undotree = {
 	"mbbill/undotree",
+	event = "BufEnter",
 	init = function()
 		vim.keymap.set(
 			"n",
@@ -142,7 +92,6 @@ local harpoon = {
 
 return {
 	dressing,
-	neotree,
 	undotree,
 	toggleterm,
 	harpoon,
