b942e59e04d471641d9d35ad1ee5ea15e42d4aa7
mrAlekD <31899692+mrAlekD@users.noreply.github.com>
2024-04-17 08:37:55 +0300

    add harpoon to which-key

--- init.lua
+++ init.lua
@@ -175,6 +175,7 @@ require("lazy").setup({
 			require("which-key").register({
 				["<leader>d"] = { name = "[D]ebug", _ = "which_key_ignore" },
 				["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
+				["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
 				["<leader>l"] = { name = "[L]sp", _ = "which_key_ignore" },
 				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
 				["<leader>t"] = { name = "[T]erminal", _ = "which_key_ignore" },
