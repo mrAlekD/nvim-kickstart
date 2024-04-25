0ee0f7926c69525ae72caa2f125a669033e8af1f
mrAlekD <31899692+mrAlekD@users.noreply.github.com>
2024-04-25 16:29:35 +0300

    refactoring of autocmds

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -1,12 +1,34 @@
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
+local function augroup(name, opts)
+	opts = opts or { clear = true }
+	name = "user-" .. name
+	return vim.api.nvim_create_augroup(name, opts)
+end
 
 -- Highlight when yanking (copying) text
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	group = augroup("highlight-yank"),
 	callback = function()
 		vim.highlight.on_yank()
+	end,
+})
+
+-- go to last loc when opening a buffer
+-- yanked from LazyVim, thank you
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = augroup("last-loc"),
+	callback = function(event)
+		local exclude = { "gitcommit" }
+		local buf = event.buf
+		if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then -- i'll leave lazyvim namings, why not
+			return
+		end
+		vim.b[buf].lazyvim_last_loc = true
+		local mark = vim.api.nvim_buf_get_mark(buf, '"')
+		local lcount = vim.api.nvim_buf_line_count(buf)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
 	end,
 })
