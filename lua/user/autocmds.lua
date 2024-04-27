a425acecdce2f09425686d8b30816d18d8dfd0f8
mrAlekD <31899692+mrAlekD@users.noreply.github.com>
2024-04-27 15:41:36 +0300

    close help etc with q

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -32,6 +32,30 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	end,
 })
 
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("close_with_q"),
+	pattern = {
+		"PlenaryTestPopup",
+		"help",
+		"lspinfo",
+		"notify",
+		"qf",
+		"query",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+		"neotest-output",
+		"checkhealth",
+		"neotest-summary",
+		"neotest-output-panel",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
+})
+
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	group = augroup("auto-create-dir"),
