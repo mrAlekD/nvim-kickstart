eedb52f2e5f08907985086f47024f79cd2bc711c
mrAlekD <31899692+mrAlekD@users.noreply.github.com>
2024-05-16 21:53:16 +0300

    commented out lines that are present by default in nvim >0.10

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -1,6 +1,6 @@
 return {
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-	{ "numToStr/Comment.nvim", opts = {} },
+	-- { "numToStr/Comment.nvim", opts = {} },
 	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
 		"lewis6991/gitsigns.nvim",
 		opts = {
--- lua/user/plugins/lsp.lua
+++ lua/user/plugins/lsp.lua
@@ -95,7 +95,7 @@ return {
 
 					-- Opens a popup that displays documentation about the word under your cursor
 					--  See `:help K` for why this keymap.
-					map("K", vim.lsp.buf.hover, "Hover Documentation")
+					-- map("K", vim.lsp.buf.hover, "Hover Documentation")
 					map("<leader>k", vim.lsp.buf.hover, "Hover Documentation")
 					vim.keymap.set(
 						"i",
